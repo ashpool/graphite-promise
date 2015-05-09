e86cfa4833c2661abf9aa418ca02ac6017dc8a57
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-05-09 20:03:42 +0200

    Support api key

--- test/carbon.js
+++ test/carbon.js
@@ -1,37 +1,52 @@
 /*jshint undef:false */
 var chaiAsPromised = require('chai-as-promised'),
-    chai = require('chai');
+  chai = require('chai');
 
 chai.should();
 chai.use(chaiAsPromised);
 
-describe('CarbonClient', function () {
-    var Carbon = require('./../lib/carbon'),
-        client = new Carbon({dsn: 'plaintext://127.0.0.1:2003/'}),
-        socketMock = {};
+describe('CarbonClient', function() {
+  var Carbon = require('./../lib/carbon'),
+    client,
+    socketMock;
 
-    describe('#write', function () {
-        before(function () {
-            client._socket = socketMock;
-        });
-
-        it('writes flattened metrics encoded as utf-8', function (done) {
-            var metric = {'home.indoor.temp': 21.2},
-                timestamp = 1427727486200;
-            socketMock.write = function (lines, encoding, cb) {
-                lines.should.equal('home.indoor.temp 21.2 1427727486200\n');
-                encoding.should.equal('utf-8');
-                cb();
-            };
-            client.write(metric, timestamp).should.eventually.equal('home.indoor.temp 21.2 1427727486200\n').notify(done);
-        });
-        it('rejects when a socket error occur', function (done) {
-            var metric = {'home.indoor.temp': 21.2},
-                timestamp = 1427727486200;
-            socketMock.write = function (lines, encoding, cb) {
-                cb(new Error('fail'));
-            };
-            client.write(metric, timestamp).should.eventually.be.rejected.notify(done);
-        });
+  describe('#write', function() {
+    beforeEach(function() {
+      client = new Carbon({ url: 'plaintext://127.0.0.1:2003/' });
+      socketMock = {};
+      client._socket = socketMock;
     });
+
+    it('writes api key on socket', function(done) {
+      var metric = { 'home.indoor.temp': 21.2 },
+        timestamp = 1427727486200;
+      socketMock.write = function(lines, encoding, cb) {
+        lines.should.equal('YOUR-API-KEY.foo 1.2\nhome.indoor.temp 21.2 1427727486200\n');
+        encoding.should.equal('utf-8');
+        cb();
+      };
+      client = new Carbon({ apiKey: 'YOUR-API-KEY.foo 1.2\n', url: 'plaintext://127.0.0.1:2003/' });
+      client._socket = socketMock;
+      client.write(metric, timestamp).should.eventually.equal('home.indoor.temp 21.2 1427727486200\n').notify(done);
+    });
+
+    it('writes flattened metrics encoded as utf-8', function(done) {
+      var metric = { 'home.indoor.temp': 21.2 },
+        timestamp = 1427727486200;
+      socketMock.write = function(lines, encoding, cb) {
+        lines.should.equal('home.indoor.temp 21.2 1427727486200\n');
+        encoding.should.equal('utf-8');
+        cb();
+      };
+      client.write(metric, timestamp).should.eventually.equal('home.indoor.temp 21.2 1427727486200\n').notify(done);
+    });
+    it('rejects when a socket error occur', function(done) {
+      var metric = { 'home.indoor.temp': 21.2 },
+        timestamp = 1427727486200;
+      socketMock.write = function(lines, encoding, cb) {
+        cb(new Error('fail'));
+      };
+      client.write(metric, timestamp).should.eventually.be.rejected.notify(done);
+    });
+  });
 });

de68e9528086755c70fb980da219517fad1f85ed
Magnus Ljadas <magnus.ljadas@gmail.com>
2016-04-15 21:36:53 +0200

    Introduce editorconfig
    Stolen from https://github.com/facebook/react/blob/master/.editorconfig

--- test/metric.js
+++ test/metric.js
@@ -1,29 +1,29 @@
 /*jshint undef:false */
 var chaiAsPromised = require('chai-as-promised'),
-    chai = require('chai'),
-    metric = require('./../lib/metric');
+  chai = require('chai'),
+  metric = require('./../lib/metric');
 
 chai.should();
 chai.use(chaiAsPromised);
 
 describe('metric', function () {
-    describe('#flatten', function () {
-        it('flattens objects', function () {
-            metric.flatten({home: {indoor: {temp: 21.2}}}).should.deep.equal({'home.indoor.temp': 21.2});
-        });
-        it('flattens objects two properties', function () {
-            metric.flatten({home: {indoor: {temp: 21.2, humidity: 94.5}}})
-                .should.deep.equal({'home.indoor.humidity': 94.5, 'home.indoor.temp': 21.2});
-        });
-        it('flattens objects three properties', function () {
-            metric.flatten({foz: {baz: {foo: 42, bar: 1337, soz: 101}}})
-                .should.deep.equal({'foz.baz.bar': 1337, 'foz.baz.foo': 42, 'foz.baz.soz': 101});
-        });
-        it('handles empty objects', function () {
-            metric.flatten({}).should.deep.equal({});
-        });
-        it('handles shallow objects', function () {
-            metric.flatten({temp: 27.1}).should.deep.equal({temp: 27.1});
-        });
+  describe('#flatten', function () {
+    it('flattens objects', function () {
+      metric.flatten({home: {indoor: {temp: 21.2}}}).should.deep.equal({'home.indoor.temp': 21.2});
     });
+    it('flattens objects two properties', function () {
+      metric.flatten({home: {indoor: {temp: 21.2, humidity: 94.5}}})
+        .should.deep.equal({'home.indoor.humidity': 94.5, 'home.indoor.temp': 21.2});
+    });
+    it('flattens objects three properties', function () {
+      metric.flatten({foz: {baz: {foo: 42, bar: 1337, soz: 101}}})
+        .should.deep.equal({'foz.baz.bar': 1337, 'foz.baz.foo': 42, 'foz.baz.soz': 101});
+    });
+    it('handles empty objects', function () {
+      metric.flatten({}).should.deep.equal({});
+    });
+    it('handles shallow objects', function () {
+      metric.flatten({temp: 27.1}).should.deep.equal({temp: 27.1});
+    });
+  });
 });

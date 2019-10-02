11341df2992ad0dc1eec5f0cf98f6c4e9dc700fe
Magnus Ljadas <magnus.ljadas@gmail.com>
2019-10-02 16:43:36 +0200

    Add optional socket to write method

--- src/carbon.ts
+++ src/carbon.ts
@@ -7,23 +7,18 @@ export default class CarbonClient {
 
   hostedGraphiteKey: string;
 
-  socket: net.Socket | undefined;
-
   constructor(config: Config) {
     this.url = config.url;
-    this.hostedGraphiteKey = config.hostedGraphiteKey ? `${config.hostedGraphiteKey}.`: '';
+    this.hostedGraphiteKey = config.hostedGraphiteKey ? config.hostedGraphiteKey + '.' : '';
   }
 
-  public async write(message: string): Promise<string> {
-    const socket = await this.connect();
+  public async write(message: string, connection?: net.Socket): Promise<string> {
+    const socket = connection || await this.connect();
     socket.write(this.hostedGraphiteKey + message, 'utf-8', () => socket.end());
     return message;
   };
 
   private async connect(): Promise<net.Socket> {
-    if (this.socket && !this.socket.destroyed) {
-      return this.socket;
-    }
     const dsn = url.parse(this.url);
     const host = dsn.hostname || 'localhost';
     const port = dsn.port ? parseInt(dsn.port, 10) : 2003;
@@ -40,18 +35,10 @@ export default class CarbonClient {
       throw(err);
     });
 
-    this.socket = socket.connect(port, host, (err?: Error) => {
+    return socket.connect(port, host, (err?: Error) => {
       if (err) {
         throw(err);
       }
     });
-
-    return this.socket;
-  };
-
-  public async end() {
-    if (this.socket) {
-      this.socket.end();
-    }
   };
 }
--- test/carbon.test.ts
+++ test/carbon.test.ts
@@ -1,8 +1,8 @@
 import * as net from 'net';
 import {anyFunction, anyString, instance, mock, verify, when} from 'ts-mockito';
 import chaiAsPromised from 'chai-as-promised';
-import * as chai from "chai";
-import CarbonClient from "../src/carbon";
+import * as chai from 'chai';
+import CarbonClient from '../src/carbon';
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -20,10 +20,9 @@ describe('CarbonClient', () => {
     it('writes api key on socket', async () => {
       // Given
       const client = new CarbonClient({hostedGraphiteKey: 'YOUR-API-KEY', url: 'plaintext://127.0.0.1:2003/'});
-      client.socket = socket;
 
       // When
-      await client.write('home.indoor.temp 21.2 1427727486200\n');
+      await client.write('home.indoor.temp 21.2 1427727486200\n', socket);
 
       // Then
       verify(socketMock.write('YOUR-API-KEY.home.indoor.temp 21.2 1427727486200\n', 'utf-8', anyFunction())).called();
@@ -32,10 +31,9 @@ describe('CarbonClient', () => {
     it('writes flattened metrics encoded as utf-8', async () => {
       // Given
       const client = new CarbonClient({url: 'plaintext://127.0.0.1:2003/'});
-      client.socket = socket;
 
       // When
-      await client.write('home.indoor.temp 21.2 1427727486200\n');
+      await client.write('home.indoor.temp 21.2 1427727486200\n', socket);
 
       // Then
       verify(socketMock.write('home.indoor.temp 21.2 1427727486200\n', 'utf-8', anyFunction())).called();
@@ -45,10 +43,9 @@ describe('CarbonClient', () => {
       // Given
       when(socketMock.write(anyString(), anyString(), anyFunction())).thenThrow(new Error());
       const client = new CarbonClient({url: 'plaintext://127.0.0.1:2003/'});
-      client.socket = socket;
 
       // When
-      client.write('home.indoor.temp 21.2 1427727486200\n').should.eventually.be.rejected.notify(done);
+      client.write('home.indoor.temp 21.2 1427727486200\n', socket).should.eventually.be.rejected.notify(done);
     });
   });
 });
